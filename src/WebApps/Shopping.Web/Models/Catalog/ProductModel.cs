d335ca9b99bbf41324ffd18647429d537532c4ac
Usman Iqbal <uiqbal.edu@gmail.com>
2024-12-03 18:53:46 -0600

    Come applcaion finish

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; }
+    public List<string> Category { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+}
+
+public record GetProductsResponse(IEnumerable<ProductModel> Products);
+public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+public record GetProductByIdResponse(ProductModel Product);
---
+++ src/WebApps/Shopping.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public record OrderModel(Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressDto ShippingAddress,
+    AddressDto BillingAddress,
+    PaymentDto Payment,
+    OrderStatus Status,
+    List<OrderItemDto> OrderItems);
+public record OrderItemDto(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+
+public record AddressDto(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country, string State, string ZipCode);
+public record PaymentDto(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending = 2,
+    Completed = 3,
+    Cancelled = 4
+}
+
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
+
